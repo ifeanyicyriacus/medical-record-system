48b5987740de7fc8e37c93614dd66245674a58d0
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-04-08 23:18:50 +0100

    Progress (edit-10 - comitting shelf files)

--- backend-java/src/main/java/com/onemedic/controllers/SuperAdminController.java
+++ backend-java/src/main/java/com/onemedic/controllers/SuperAdminController.java
@@ -8,7 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/sudo/{id}")
+@RequestMapping("/api/sudo/{id}/admins")
 public class SuperAdminController {
     private final SuperAdminService superAdminService;
 
@@ -16,27 +16,27 @@ public class SuperAdminController {
         this.superAdminService = superAdminService;
     }
 
-    @PostMapping("/admins")
-    public Admin createAdmin(@RequestBody Admin admin) {
-        return superAdminService.createAdmin(admin);
-    }
-
-    @GetMapping("/admins")
+    @GetMapping
     public Page<Admin> getAllAdmins(Pageable pageable, @PathVariable String id) {
         return this.superAdminService.getAllAdmins(pageable);
     }
 
-    @GetMapping("/admins?id={adminId}")
-    public Admin getAdmin(@PathVariable String id, @PathVariable String adminId) {
+    @PostMapping
+    public Admin createAdmin(@RequestBody Admin admin) {
+        return superAdminService.createAdmin(admin);
+    }
+
+    @GetMapping("?id={adminId}")
+    public Admin getAdminById(@PathVariable String id, @PathVariable String adminId) {
         return superAdminService.getAdminById(adminId);
     }
 
-    @GetMapping("/admins?email={email}")
+    @GetMapping("?email={email}")
     public Admin getAdminByEmail(@PathVariable String email, @PathVariable String id) {
         return superAdminService.getAdminByEmail(email);
     }
 
-    @PutMapping("admins/{adminId}")
+    @PutMapping("/{adminId}")
     public Admin updateAdmin(@PathVariable String id, @PathVariable String adminId,
                              @RequestBody Admin adminDetails) {
         return superAdminService.updateAdmin(adminId, adminDetails);
