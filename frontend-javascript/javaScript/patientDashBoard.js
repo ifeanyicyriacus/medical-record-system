77ccf1bc44954431e33920309940f21143eb7e47
ighoEmmanuel <ighoe571@gmail.com>
2025-04-08 23:36:49 +0100

    working on patient dash board

---
+++ frontend-javascript/javaScript/patientDashBoard.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const lastVitalCheck = "20°C / 70Kg |160- 170H/g |27 Bml"
+    const vitalList = document.getElementById("vital-check");
+        vitalList.innerHTML = "";
+        vitalList.textContent = lastVitalCheck
+
+});
